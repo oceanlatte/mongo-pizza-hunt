4a3378fabb785f18ae404ee47b97ac1d0dab289d
ocean <osciris.veliz@gmail.com>
2022-09-14 17:09:32 -0500

    add skeleton routes for pizza-routes.js

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
+
+// set up for GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+
+// set up for GET one, PUT, & DELETE at /api/pizzas/:id
+router 
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
+
