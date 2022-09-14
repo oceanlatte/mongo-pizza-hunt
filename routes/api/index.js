f54f18b1a00bfeecb1dbea8d34b80139caa12d6a
ocean <osciris.veliz@gmail.com>
2022-09-14 17:14:06 -0500

    add api index.js file and integrate API routes into the server

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of '/pizzas' to routes created in 'pizza-routes'
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
+
