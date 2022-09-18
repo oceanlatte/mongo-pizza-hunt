f4a83ce4a5dde627d51a82b79b536e2a6ea4f2ed
ocean <osciris.veliz@gmail.com>
2022-09-17 22:31:09 -0500

    change mongodb URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // tells Mongoose which DB to connect to
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
