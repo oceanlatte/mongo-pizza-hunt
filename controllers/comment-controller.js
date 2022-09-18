a04b6ef73bed8f6ed4f6969e26a1e21ede3838cf
ocean <osciris.veliz@gmail.com>
2022-09-17 20:59:36 -0500

    update the comment controller and routes with replies info

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,21 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
+  addReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found whit that id! '});
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -42,6 +57,17 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
+      .catch(err => res.json(err));
+  },
+
+  // remove replies 
+  removeReply( { params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+      )
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.json(err));
   }
 };
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 const {
   addComment,
-  removeComment
+  removeComment,
+  addReply,
+  removeReply
 } = require('../../controllers/comment-controller');
 
 // route for: /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // route for: /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// route for deleting a reply. Needs: pizzaId, commentId, replyId
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 
 module.exports = router;
